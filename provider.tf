d0a258285b7ac93df4973fa18888d8c704f322f6
Ashutoshh <asashuthegreat4@gmail.com>
2022-07-12 15:46:47 +0530

    Update provider.tf

--- provider.tf
+++ provider.tf
@@ -1,7 +1,7 @@
 provider "aws" {
 
-    access_key = "AKIAQ4OUTXWCXAMXT2AM"
-    secret_key = "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"
+    access_key = ""
+    secret_key = ""
     region = "us-west-2" # this aws oregon region 
    
 }
